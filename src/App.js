be52330982ffc8d56e431d2038fdbd668d7f106c
Owais Badat <owaisbadat@gmail.com>
2018-11-27 20:54:29 -0600

    book barn lib

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Booklist from './components/Booklist'
+
+const BOOKS_URL = "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // initialize the state
+    this.state = {
+      books : [],
+    }
+  }
+
+
+  componentDidMount() {
+
+    fetch(BOOKS_URL)
+      .then(res => res.json())
+      .then(data => this.setState({ books: data}))
+
+  }
+
   render() {
+    console.log(this.state.books);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Booklist books = {this.state.books} />
       </div>
     );
   }
